789c36ad42ca221a4b33a72725f5d968b6a48f55
Siham Tabik <stabik@uma.es>
2018-02-12 10:31:44 +0100

    Add files via upload

--- convolutional.py
+++ convolutional.py
@@ -1,153 +1,90 @@
 
-<!-- saved from url=(0029)https://www.ac.uma.es/~siham/ -->
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-        <title>Siham Tabik</title>
-    </head>
-<body lang="ES">
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
 
-<img src="./Siham Tabik_files/me.jpg" style="float: right">
+batch_size = 128
+test_size = 256
+img_size = 28
+num_classes = 10
 
-<h1>Siham Tabik</h1>
-Assistant Professor <br><br>
+def init_weights(shape):
+	return tf.Variable(tf.random_normal(shape, stddev=0.01))
 
-Current affiliation:
-<font size="1">
-<ul>
-<a href="http://www.ac.uma.es/">Dpt. Informatics and Systems Engineering</a><br> 
-Ed. Ada Byron<br>C\ Maria de Luna 1<br>E-50018 Zaragoza<br> Spain<br>
-Email: stabik AT unizar.es<br>
-Office: D.0.0.1<br>
-Phone : (+34) 876555545	
-<br><br>
-</ul>
-</font>
-Former affiliation:
-<font size="1">
-<ul>
-<a href="http://www.ac.uma.es/">Department of Computer Architecture</a><br> 
-University of Málaga<br>
-E-29080 Málaga<br>
+def model(X, w, w2, w3, w4, w_o, p_keep_conv, p_keep_hidden):
+	conv1 = tf.nn.conv2d(X, w,strides=[1, 1, 1, 1],padding='SAME')
+	conv1_a = tf.nn.relu(conv1)
+	conv1 = tf.nn.max_pool(conv1_a, ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding='SAME')
+	conv1 = tf.nn.dropout(conv1, p_keep_conv)
+	conv2 = tf.nn.conv2d(conv1, w2,strides=[1, 1, 1, 1],padding='SAME')
+	conv2_a = tf.nn.relu(conv2)
+	conv2 = tf.nn.max_pool(conv2_a, ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding='SAME')
+	conv2 = tf.nn.dropout(conv2, p_keep_conv)
+	conv3=tf.nn.conv2d(conv2, w3,strides=[1, 1, 1, 1],padding='SAME')
+	conv3 = tf.nn.relu(conv3)
+	FC_layer = tf.nn.max_pool(conv3, ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding='SAME')
+	FC_layer = tf.reshape(FC_layer, [-1, w4.get_shape().as_list()[0]])
+	FC_layer = tf.nn.dropout(FC_layer, p_keep_conv)
+	output_layer = tf.nn.relu(tf.matmul(FC_layer, w4))
+	output_layer = tf.nn.dropout(output_layer, p_keep_hidden)
+	result = tf.matmul(output_layer, w_o)
+	return result
 
+mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
 
-Email: stabik AT uma.es<br>
-Office: 2.2.25<br>
-Phone: (+34) 952 13 41 69<br>
-</ul>
-</font>
-<br>
+trX = trX.reshape(-1, img_size, img_size, 1)
+teX = teX.reshape(-1, img_size, img_size, 1)
+# 28x28x1 input img
+# 28x28x1 input img
+X = tf.placeholder("float", [None, img_size, img_size, 1])
+Y = tf.placeholder("float", [None, num_classes])
+#Fill w, w2, w3, w4 and w_o with the right dimensions
+w = init_weights([?, ?, ?, ?])
 
-<h2>Research Interests</h2>
-<ul>
-    <li>Developing novel and scalable algorithms for Geographic Information Systems
-    </li><li>Optimizing scientific applications for hybrid and heterogeneous systems including Supercomputers
-    </li><li>Accelerating scientific kernels on GPUs, many-core and multi-core  
-</li><li>Building performance models for specific computation patterns
-
-</li></ul>
-
-
-
-<h2>Selected Publications</h2>    
-<ul>
-
-<li> S Tabik, M Peemen, N Guil, H Corporaal. <br> <font color="blue"> Demystifying the 16x16 thread-block for stencils on the GPU</font>  <br> <em>
-Concurrency and Computation: Practice and Experience </em>, 27 (18), 5557-5573, 2015. <a href="https://www.ac.uma.es/~siham/CPE.pdf">[pdf]</a><br><br>
-
-</li><li> A. R. Cervilla, S. Tabik, L. F. Romero. <br> <font color="blue"> Sitting Multiple Observers for Maximum Coverage: An Accurate Approach </font>  <br> <em>
-Procedia Computer Science, Volume 51, 2015, Pages 356-365. <a href="https://www.ac.uma.es/~siham/ICCS.pdf">[pdf]</a></em><br><br>
+w2 = init_weights([?, ?, ?, ?])
+w3 = init_weights([?, ?, ?, ?])
+w4 = init_weights([? *? * ?, ?])
+w_o = init_weights([?,?])
 
 
+p_keep_conv = tf.placeholder("float")
+p_keep_hidden = tf.placeholder("float")
+py_x = model(X, w, w2, w3, w4, w_o, p_keep_conv, p_keep_hidden)
+Y_ = tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y)
 
-</li><li> S. Tabik, A.R. Cervilla, E, Zapata, L.F. Romero. <br> <font color="blue"> Efficient Data Structure and Highly Scalable Algorithm for Total-Viewshed Computation </font> <br> <em>IEEE Journal of Selected Topics in Applied Earth Observations and Remote Sensing</em>, 8(1):304-310, 2015. <a href="https://www.ac.uma.es/~siham/STARS.pdf">[pdf] </a><br><br>
+cost = tf.reduce_mean(Y_)
 
-</li><li> S Tabik, G. Ortega, E.M. Garzon  <br><font color="blue"> Performance Evaluation of Kernel Fusion BLAS Routines on the GPU: Iterative Solvers as Case Study</font>,<br><em> 
-The Journal of Supercomputing, 2014. <a href="https://www.ac.uma.es/~siham/FusionTrans.pdf">[pdf]</a></em><a href="https://www.ac.uma.es/~siham/FusionTrans.pdf">. </a><br><br>
+optimizer = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
 
+predict_op = tf.argmax(py_x, 1)
 
-</li><li> S. Tabik, L.F. Romero, E. L. Zapata. <br>
-        <font color="blue"> Simultaneous Computation of Total Viewshed on Large High Resolution Grids</font>,<br><em> International Journal of Geographical Information
-        Science</em>, 27(4): 804-814, 2013. <a href="https://www.ac.uma.es/~siham/visibility-2012.pdf">[pdf]</a><br><br>
+with tf.Session() as sess:
+   	init_g = tf.global_variables_initializer()
+	sess.run(init_g)
+	for i in range(100):
+		training_batch =zip(range(0, len(trX),
+			batch_size),
+			range(batch_size,
+			len(trX)+1,
+			batch_size))
 
+		for start, end in training_batch:
+			sess.run(optimizer , feed_dict={X: trX[start:end],
+				Y: trY[start:end],
+				p_keep_conv: 0.8,
+				p_keep_hidden: 0.5})
+		test_indices = np.arange(len(teX)) # Get A Test Batch
+		np.random.shuffle(test_indices)
+		test_indices = test_indices[0:test_size]
 
-         </li><li> S Tabik, A Villegas, EL Zapata, LF Romero  <br><font color="blue"> Optimal tilt and orientation maps: a multi-algorithm approach for heterogeneous multicore-GPU systems</font>,<br><em> 
-        The Journal of Supercomputing, 1-13, 2013. <a href="https://www.ac.uma.es/~siham/max-map.pdf">[pdf]</a></em><br><br> 
-
-
-        </li><li> S. Tabik, L.F. Romero, P. Mimica, O. G. Plata, E. L. Zapata <br><font color="blue">VLBI-resolution radio-map algorithms:
-Performance analysis of different levels of data-sharing on multi-socket,
-multi-core architectures</font>,<br><em> Computer Physics Communications
-183(9): 1937-1946, 2012.</em> <a href="https://www.ac.uma.es/~siham/cpc-2012.pdf">[pdf]</a><br><br>
-
-       
-        </li><li> G. Utrera, S. Tabik, J. Corbalan, J. Labarta. <br>
-        <font color="blue"> A Job Scheduling Approach for Multi-core Clusters Based on Virtual Malleability</font>,<br><em>
-       Euro-Par</em>, pp. 191-203, August 27-31, 2012. <a href="https://www.ac.uma.es/~siham/FCFS-VM.pdf">[pdf]</a><br><br>
-
-
-
-</li><li> S. Tabik, L.F. Romero, E. L. Zapata. 
-
-<br><font color="blue"> High Performance Three-horizon Composition Algorithm for Large Scale Terrains</font>, <br>
-<em> International Journal of Geographical Information Science </em>, Vol. 25, Issue 4, pp. 541-555, 2011. <a href="https://www.ac.uma.es/~siham/horizon.pdf">[pdf]</a><br><br>
-
-<!----        <LI> S. Tabik, L.F. Romero, G. Utrera and O. Plata. <BR>
-<FONT  COLOR="blue"> Mapping Parallel Loops on Multicore Systems </FONT></A> <BR><EM> 15th Workshop on Compilers for Parallel Computing (CPC'10)</EM>, Vienna (Austria), June 2010.<a href=""></A> <BR><BR>
----->	
-
-        </li><li> A. Navarro, R. Asenjo, S. Tabik, C. Cascaval. <br> 
-	<font color="blue"> Analytical Modeling of Pipeline Parallelism. </font> .<br> <em>
-International Conference on Parallel Architectures and Compilation Techniques (PACT'09), pp. 281-290, September 12-16, 2009. <a href="https://www.ac.uma.es/~siham/pact09_workstealing.pdf">[pdf]</a><br><br>
-
-	
-
-        </em></li><li><em> S. Tabik, L.F. Romero, J. Vias, Emilio L. Zapata.<br>
-        <font color="blue"> Fast Clear-sky Solar Irradiation Computation for Very Large Digital Elevation Models</font>.<br>
-        <em> Computer Physics Communications </em>, Vol. 178, Issue 11, pp. 800-808, 2008. <a href="https://www.ac.uma.es/~siham/Irradiation.pdf">[pdf]</a><br><br>
+		print(i, np.mean(np.argmax(teY[test_indices], axis=1) ==
+		sess.run
+		(predict_op,
+		feed_dict={X: teX[test_indices],
+		Y: teY[test_indices],
+		p_keep_conv: 1.0,
+		p_keep_hidden: 1.0})))
+		
 
 
-
-        </em></li><li><em> S. Tabik, L.F. Romero, E.M. Garzon, J.I. Ramos.<br>
-        <font color="blue">  On a Model of Three-dimensional Bursting and its Parallel Implementations</font>.<br>
-        <em> Computer Physics Communications </em>, Vol. 178, Issue 7, pp. 471-485, 2008. <a href="https://www.ac.uma.es/~siham/bursting.pdf">[pdf]</a><br><br>
-
-
-        </em></li><li><em> S. Tabik, E.M. Garzon, I. Garcia, J.J. Fernandez.<br>
-        <font color="blue"> High Performance Noise Reduction for Biomedical Multidimensional Data</font>.<br>
-        <em> Digital Signal Processing </em>, Vol. 17, Issue 4, pp. 724-736, 2007. <a href="https://www.ac.uma.es/~siham/AND.pdf">[pdf]</a><br>
-
- </em></li></ul><em>
-
-
-
-<h2>Software</h2>    
-<ul>
-
-<li> <font color="blue"> An efficient Total-viewshed Software for Linux OS <a href="https://www.ac.uma.es/~siham/tvs.tar.gz">[Download] </a></font><br>
-
-</li><li> <font color="blue"> Insolator: High Performance GIS-Software for Solar
-Irradiation Computation <a href="https://www.ac.uma.es/~siham/research/radiation.tar.gz">[Download] </a></font><br>
-Insolator is an open source, high performance software for computing solar energy input that can be collected <br> 
-at any point of a terrain represented by a DEM (Digital Elevation Model). It is stable and can be run on distributed <br> and or shared memory systems. <br>
-<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Licencia
-Creative Commons" style="border-width:0" src="./Siham Tabik_files/88x31.png"></a><br><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">Insolator: High Performance GIS-Software for Solar
-Irradiation Computation</span> <a xmlns:cc="http://creativecommons.org/ns#" href="http://www.ac.uma.es/~siham/" property="cc:attributionName" rel="cc:attributionURL"></a> under
-Licence <br><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons
-Reconocimiento-NoComercial-CompartirIgual 3.0 Unported</a>.<br>. <a xmlns:dct="http://purl.org/dc/terms/" href="http://www.ac.uma.es/~siham/research/radiation.tar.gz" rel="dct:source">www.ac.uma.es</a>.
-<br><br>
-</li><li> <font color="blue"> Web appication based in part on Insolator <a href="http://150.214.109.57/radiacion/">[http] </a></font><br>
-This application is available for the area of Malaga.
-</li></ul>
-
-<h2>Technical Reports</h2>
-<li>  S. Tabik, L.F. Romero.
-<br><font color="blue"><a href="https://www.ac.uma.es/~siham/epsilon.pdf">The horizontal error in horizon computation. </a></font><a href="https://www.ac.uma.es/~siham/epsilon.pdf"> </a><br>
-
-
-<p> </p><hr> <img src="./Siham Tabik_files/dac-pica-small.gif" border="0" width="62" height="61" hspace="10" align="left"> <font size="2"><i> <a href="http://www.ac.uma.es/">Dept. Arquitectura de Computadores</a>,
-<a href="http://www.uma.es/">UMA</a><br>
-<br clear="all"> <hr>
-
-
-
-
-</i></font></li></em></body></html>
